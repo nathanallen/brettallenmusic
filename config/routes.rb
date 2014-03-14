faf0e675f8ec391d9ec58b374952d3d189216347
nathanallen <thenathanator@gmail.com>
2014-03-13 22:22:18 -0700

    stripe views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Brettallenmusic::Application.routes.draw do
   resources 'catalog'
+  resources 'charges'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
