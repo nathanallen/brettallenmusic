e2d9253bad962033510e6f38398b14afb7009f5e
nathanallen <thenathanator@gmail.com>
2014-03-13 22:11:24 -0700

    collect customer shipping address

--- app/assets/javascripts/catalog.js
+++ app/assets/javascripts/catalog.js
@@ -6,7 +6,8 @@ $('document').ready(function(){
 
   handler = StripeCheckout.configure({
     key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-    //image: '/square-image.png',
+    shippingAddress: true,
+    //image: '/kcleff-icon-or-sheetmusic.png',
     token: function(token, args) {
       // Use the token to create the charge with a server-side script.
       // You can access the token ID with `token.id`
