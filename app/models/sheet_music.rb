970d9063b4e93d1fb481ccdbb41426285900d4a5
nathanallen <thenathanator@gmail.com>
2014-03-13 17:25:55 -0700

    more helper methods for view

--- app/models/sheet_music.rb
+++ app/models/sheet_music.rb
@@ -17,14 +17,4 @@ class SheetMusic < ActiveRecord::Base
     arrangements.first.parts.count > 2
   end
 
-  # def get_arrangements
-  #   if arrangements.count > 1 #multiple_arrangements
-  #     return arrangements.map{|a| a.version }
-  #   elsif arrangements.first.parts.count > 2 #multiple_parts
-  #     return arrangements.first.parts.map{|part| part.instrument }
-  #   else
-  #     return arrangements.first.version #one_arrangement
-  #   end
-  # end
-
 end
